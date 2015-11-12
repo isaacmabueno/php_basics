b1be8683575771653a3b8831a4fcd22ca0387a98
Isaac Bueno <isaacmabueno@gmail.com>
2015-11-12 14:39:05 -0600

    files for dayzzzz

--- code.php
+++
@@ -1,2 +0,0 @@
-<?php echo "T-Shirts"; ?>
-<p>The currect server time is <?php echo date("H:i:s: a"); ?>.</p>
---
+++ shirts.php
@@ -0,0 +1,5 @@
+<?php include ('inc/header.php'); ?>
+  <div class="section page">
+    <h1>Mike's Full Catelog of Shirts</h1>
+  </div>
+<?php include('inc/footer.php'); ?>
