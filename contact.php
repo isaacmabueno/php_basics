74b92050051a72ad6b4df8ee7195fcdb5628540d
Isaac Bueno <isaacmabueno@gmail.com>
2015-11-15 16:44:00 -0600

    contact form

--- contact.php
+++ contact.php
@@ -5,7 +5,40 @@ include("inc/header.php");
 ?>
 
 <div class="section page">
-  <h1>Contact</h1>
+  <div class="wrapper">
+      <h1>Contact</h1>
+      <p>
+        I'd love to hear from you! Complete the form, I would love to hear from you!</p>
+        <form method="post">
+          <table>
+            <tr>
+              <th>
+                <label for="name">Name</label>
+              </th>
+              <td>
+                <input type="text" name="name" id="name">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label for="email">Email</label>
+              </th>
+              <td>
+                <input type="text" name="email" id="email">
+              </td>
+            </tr>
+            <tr>
+              <th>
+                <label for="message">Message</label>
+              </th>
+              <td>
+                <textarea name="message" id="message"></textarea>
+              </td>
+            </tr>
+          </table>
+          <input type="submit" value="Send">
+        </form>
+  </div>
 
 </div>
 <?php include('inc/footer.php'); ?>
